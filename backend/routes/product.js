347ef4bb82e433a28518b7b637d40dcc85c2c890
KhaledAlhamad <khaledalhamad997@gmail.com>
2021-12-19 10:52:43 +0300

    updated product route

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,95 +1,82 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
+const {
+  generateAccessToken,
+  verifyAccess,
+  verifyAndAuthorize,
+  verifyAdmin,
+} = require("../auth/auth");
 
-const Product = require('../models/Product').Product;
-
-// get all products 
-router.get('/', async (req,res) => {
-    Product.find({} , (err,products) => {
-      if(products.length){
-        res.status(200).send(products)
-      } else{
-        res.status(400).send("No products available")
-      }  
-    });
-})
+const Product = require("../models/Product").Product;
 
 //get specific product
-router.get('/:id', async (req,res) => {
+router.get("/:id", async (req, res) => {
   const id = req.params.id;
-  Product.find({} , (err,products) => {
-    if(products){
-      Product.find({_id : id} , (err,products) => {
-        if(products){
-          res.status(200).send(products)
-        } else{
-          res.status(400).send("Product NOT found")
-        }  
+  Product.find({}, (err, products) => {
+    if (products) {
+      Product.find({ _id: id }, (err, products) => {
+        if (products) {
+          res.status(200).send(products);
+        } else {
+          res.status(400).send("Product NOT found");
+        }
       });
-    } else{
-      res.status(400).send("No products available")
-    }  
+    } else {
+      res.status(400).send("No products available");
+    }
   });
+});
 
-  
-})
+// get all products
+router.get("/", async (req, res) => {
+  Product.find({}, (err, products) => {
+    if (products.length) {
+      res.status(200).send(products);
+    } else {
+      res.status(400).send("No products available");
+    }
+  });
+});
 
-//Add product
-router.post('/', async (req,res) => {
-    const {name, image, quantity, category , color} = req.body;
-    // console.log(quantity)
+//ADD Product
+router.post("/", verifyAdmin, async (req, res) => {
+  const newProduct = new Product(req.body);
 
-    const newProduct = await Product.create({name, image, quantity, category , color})
-    if (newProduct) {
-        res.status(201).json({
-          _id: newProduct._id,
-          name: newProduct.name,
-          image: newProduct.image,
-          quantity: newProduct.quantity,
-          category: newProduct.category,
-          color: newProduct.color
-        });
-      } else {
-        res.status(400);
-        throw new Error("Invalid user data");
-      }
-    // res.send(product)
-})
+  try {
+    const savedProduct = await newProduct.save();
+    res.status(200).json(savedProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//UPDATE Product
+router.put("/:id", verifyAdmin, async (req, res) => {
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 //Delete specific product
-router.delete('/:id', async (req,res) => {
-    Product.findOneAndDelete({_id : req.params.id} , async (err,product) => {
-      if(product){
-        res.status(200).send({ status: "success", message: "Product Deleted Successfully" })
-      } else{
-        res.status(400).send("Couldn NOT delete product")
-      }  
-    });
-})
-
-//update product
-router.put('/:name', (req,res) => {
-  const name = req.params.name;
-
-  Product.updateOne(
-    { name: name },
-    {
-      name: req.body.name,
-      image: req.body.pages,
-      quantity: req.body.image,
-      category: req.body.image,
-      color: req.body.image
-    },
-    (err, Products) => {
-      if (err) {
-        console.log(err);
-      }
-      Products.modifiedCount == 0
-        ? res.status(400).send("Product NOT found")
-        : res.status(200).send("Product updated");
+router.delete("/:id", verifyAdmin, async (req, res) => {
+  Product.findOneAndDelete({ _id: req.params.id }, async (err, product) => {
+    if (product) {
+      res
+        .status(200)
+        .send({ status: "success", message: "Product Deleted Successfully" });
+    } else {
+      res.status(400).send("Couldn NOT delete product");
     }
-  );
-})
-
+  });
+});
 
 module.exports = router;

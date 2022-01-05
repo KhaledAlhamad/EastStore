0ed1b7daa5aae752db645abc36d395234a0f4ff0
KhaledAlhamad <khaledalhamad997@gmail.com>
2022-01-05 15:03:30 +0300

    changed project structure

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ app.use(express.json());
 const mongoose = require('mongoose');
 const path = require("path");
 
-app.use('/', express.static(path.join(__dirname, '/frontend/build')));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "frontend/build/index.html"));
-});
+// app.use('/', express.static(path.join(__dirname, '../frontend/build')));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+// });
 
 const PORT = process.env.PORT || 8080;
 // app.listen(PORT);
